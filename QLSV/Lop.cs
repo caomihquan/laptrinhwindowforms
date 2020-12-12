20c15a19a688d58d6b58fddd989d9842ad7bce19
caomihquan <71248793+caomihquan@users.noreply.github.com>
2020-12-11 18:35:17 -0800

    update

---
+++ QLSV/Lop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.model
+{
+    public class Lop
+    {
+        public string MaLop { get; set; }
+        public string TenLop { get; set; }
+        public double SiSo { get; set; }
+        public double NamNhapHoc { get; set; }
+        public List<SinhVien> dsSV { get; set; }
+    }
+}
